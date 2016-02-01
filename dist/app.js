3bd092ccc30d5f1796df648a864441c25b160565
Tekin Ağgül <tekinaggul@gmail.com>
2016-02-01 18:10:19 +0200

    Ecmascript 6 examples loaded

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,513 @@
 "use strict";
 
-var p = 3.14;
+var _obj;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _templateObject = _taggedTemplateLiteral(["In JavaScript this is\n not legal."], ["In JavaScript this is\n not legal."]),
+    _templateObject2 = _taggedTemplateLiteral(["http://foo.org/bar?a=", "&b=", "\n     Content-Type: application/json\n     X-Credentials: ", "\n     { \"foo\": ", ",\n       \"bar\": ", "}"], ["http://foo.org/bar?a=", "&b=", "\n     Content-Type: application/json\n     X-Credentials: ", "\n     { \"foo\": ", ",\n       \"bar\": ", "}"]);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// ============================== Arrows =========================== \\
+// Expression bodies
+var odds = evens.map(function (v) {
+  return v + 1;
+});
+var nums = evens.map(function (v, i) {
+  return v + i;
+});
+var pairs = evens.map(function (v) {
+  return { even: v, odd: v + 1 };
+});
+
+// Statement bodies
+nums.forEach(function (v) {
+  if (v % 5 === 0) fives.push(v);
+});
+
+// Lexical this
+var bob = {
+  _name: "Bob",
+  _friends: [],
+  printFriends: function printFriends() {
+    var _this = this;
+
+    this._friends.forEach(function (f) {
+      return console.log(_this._name + " knows " + f);
+    });
+  }
+};
+// ============================== Classes =========================== \\
+
+var SkinnedMesh = function (_THREE$Mesh) {
+  _inherits(SkinnedMesh, _THREE$Mesh);
+
+  function SkinnedMesh(geometry, materials) {
+    _classCallCheck(this, SkinnedMesh);
+
+    var _this2 = _possibleConstructorReturn(this, Object.getPrototypeOf(SkinnedMesh).call(this, geometry, materials));
+
+    _this2.idMatrix = SkinnedMesh.defaultMatrix();
+    _this2.bones = [];
+    _this2.boneMatrices = [];
+    //...
+    return _this2;
+  }
+
+  _createClass(SkinnedMesh, [{
+    key: "update",
+    value: function update(camera) {
+      //...
+      _get(Object.getPrototypeOf(SkinnedMesh.prototype), "update", this).call(this);
+    }
+  }, {
+    key: "boneCount",
+    get: function get() {
+      return this.bones.length;
+    }
+  }, {
+    key: "matrixType",
+    set: function set(matrixType) {
+      this.idMatrix = SkinnedMesh[matrixType]();
+    }
+  }], [{
+    key: "defaultMatrix",
+    value: function defaultMatrix() {
+      return new THREE.Matrix4();
+    }
+  }]);
+
+  return SkinnedMesh;
+}(THREE.Mesh);
+
+// ============================== Enhanced Object Literals =========================== \\
+
+var obj = _obj = _defineProperty({
+  // __proto__
+  __proto__: theProtoObj,
+  // Shorthand for ‘handler: handler’
+  handler: handler,
+  // Methods
+  toString: function toString() {
+    // Super calls
+    return "d " + _get(Object.getPrototypeOf(_obj), "toString", this).call(this);
+  }
+}, 'prop_' + function () {
+  return 42;
+}(), 42);
+
+// ============================== Template Strings =========================== \\
+// Basic literal string creation
+"In JavaScript '\n' is a line-feed."
+
+// Multiline strings
+(_templateObject);
+
+// String interpolation
+var name = "Bob",
+    time = "today";
+"Hello " + name + ", how are you " + time + "?";
+
+// Construct an HTTP request prefix is used to interpret the replacements and construction
+POST(_templateObject2, a, b, credentials, foo, bar)(myOnReadyStateChangeHandler);
+
+// ============================== Destructuring =========================== \\
+// list matching
+var _ref = [1, 2, 3];
+var a = _ref[0];
+var b = _ref[2];
+
+// object matching
+
+var _getASTNode = getASTNode();
+
+var a = _getASTNode.op;
+var b = _getASTNode.lhs.op;
+var c = _getASTNode.rhs;
+
+// object matching shorthand
+// binds `op`, `lhs` and `rhs` in scope
+
+var _getASTNode2 = getASTNode();
+
+var op = _getASTNode2.op;
+var lhs = _getASTNode2.lhs;
+var rhs = _getASTNode2.rhs;
+
+// Can be used in parameter position
+
+function g(_ref2) {
+  var x = _ref2.name;
+
+  console.log(x);
+}
+g({ name: 5 });
+
+// Fail-soft destructuring
+var _ref3 = [];
+var a = _ref3[0];
+
+a === undefined;
+
+// Fail-soft destructuring with defaults
+var _ref4 = [];
+var _ref4$ = _ref4[0];
+var a = _ref4$ === undefined ? 1 : _ref4$;
+
+a === 1;
+
+// ============================== Default + Rest + Spread =========================== \\
+function f(x) {
+  var y = arguments.length <= 1 || arguments[1] === undefined ? 12 : arguments[1];
+
+  // y is 12 if not passed (or passed as undefined)
+  return x + y;
+}
+f(3) == 15;
+function f(x) {
+  // y is an Array
+  return x * (arguments.length - 1);
+}
+f(3, "hello", true) == 6;
+function f(x, y, z) {
+  return x + y + z;
+}
+// Pass each elem of array as argument
+f.apply(undefined, [1, 2, 3]) == 6;
+
+// ============================== Let + Const =========================== \\
+
+// ============================== Iterators + For..Of =========================== \\
+var fibonacci = _defineProperty({}, Symbol.iterator, function () {
+  var pre = 0,
+      cur = 1;
+  return {
+    next: function next() {
+      var _ref5 = [cur, pre + cur];
+      pre = _ref5[0];
+      cur = _ref5[1];
+
+      return { done: false, value: cur };
+    }
+  };
+});
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = fibonacci[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var n = _step.value;
+
+    // truncate the sequence at 1000
+    if (n > 1000) break;
+    console.log(n);
+  }
+
+  // ============================== Generators =========================== \\
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+var fibonacciX = _defineProperty({}, Symbol.iterator, regeneratorRuntime.mark(function _callee() {
+  var pre, cur, temp;
+  return regeneratorRuntime.wrap(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          pre = 0, cur = 1;
+
+        case 1:
+          temp = pre;
+
+          pre = cur;
+          cur += temp;
+          _context.next = 6;
+          return cur;
+
+        case 6:
+          _context.next = 1;
+          break;
+
+        case 8:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, _callee, this);
+}));
+
+var _iteratorNormalCompletion2 = true;
+var _didIteratorError2 = false;
+var _iteratorError2 = undefined;
+
+try {
+  for (var _iterator2 = fibonacciX[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+    var n = _step2.value;
+
+    // truncate the sequence at 1000
+    if (n > 1000) break;
+    console.log(n);
+  }
+  // ============================== Unicode =========================== \\
+  // same as ES5.1
+} catch (err) {
+  _didIteratorError2 = true;
+  _iteratorError2 = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion2 && _iterator2.return) {
+      _iterator2.return();
+    }
+  } finally {
+    if (_didIteratorError2) {
+      throw _iteratorError2;
+    }
+  }
+}
+
+"𠮷".length == 2;
+
+// new RegExp behaviour, opt-in ‘u’
+"𠮷".match(/(?:[\0-\t\x0B\f\x0E-\u2027\u202A-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])/)[0].length == 2;
+
+// new form
+"𠮷" == "𠮷" == "𠮷";
+
+// new String ops
+"𠮷".codePointAt(0) == 0x20BB7;
+
+// for-of iterates code points
+var _iteratorNormalCompletion3 = true;
+var _didIteratorError3 = false;
+var _iteratorError3 = undefined;
+
+try {
+  for (var _iterator3 = "𠮷"[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+    var c = _step3.value;
+
+    console.log(c);
+  }
+
+  // ============================== Module Loaders =========================== \\
+
+  // ============================== Map + Set + WeakMap + WeakSet =========================== \\
+  // Sets
+} catch (err) {
+  _didIteratorError3 = true;
+  _iteratorError3 = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion3 && _iterator3.return) {
+      _iterator3.return();
+    }
+  } finally {
+    if (_didIteratorError3) {
+      throw _iteratorError3;
+    }
+  }
+}
+
+var s = new Set();
+s.add("hello").add("goodbye").add("hello");
+s.size === 2;
+s.has("hello") === true;
+
+// Maps
+var m = new Map();
+m.set("hello", 42);
+m.set(s, 34);
+m.get(s) == 34;
+
+// Weak Maps
+var wm = new WeakMap();
+wm.set(s, { extra: 42 });
+wm.size === undefined;
+
+// Weak Sets
+var ws = new WeakSet();
+ws.add({ data: 42 });
+// Because the added object has no other references, it will not be held in the set
+
+// ============================== Proxies =========================== \\
+// Proxying a normal object
+var target = {};
+var handler = {
+  get: function get(receiver, name) {
+    return "Hello, " + name + "!";
+  }
+};
+
+var p = new Proxy(target, handler);
+p.world === 'Hello, world!';
+
+// Proxying a function object
+var target = function target() {
+  return 'I am the target';
+};
+var handler = {
+  apply: function apply(receiver) {
+    return 'I am the proxy';
+  }
+};
+
+var p = new Proxy(target, handler);
+p() === 'I am the proxy';
+
+// ============================== Symbols =========================== \\
+var MyClass = function () {
+
+  // module scoped symbol
+  var key = Symbol("key");
+
+  function MyClass(privateData) {
+    this[key] = privateData;
+  }
+
+  MyClass.prototype = {
+    doStuff: function doStuff() {
+      this[key];
+    }
+  };
+
+  return MyClass;
+}();
+
+var c = new MyClass("hello");
+c["key"] === undefined;
+
+// ============================== Subclassable Built-ins =========================== \\
+// Pseudo-code of Array
+
+var Array = function () {
+  function Array() {/* ... */
+
+    _classCallCheck(this, Array);
+  }
+
+  _createClass(Array, null, [{
+    key: Symbol.create,
+    value: function value() {
+      // Install special [[DefineOwnProperty]]
+      // to magically update 'length'
+    }
+  }]);
+
+  return Array;
+}();
+
+// User code of Array subclass
+
+var MyArray = function (_Array) {
+  _inherits(MyArray, _Array);
+
+  function MyArray() {
+    var _Object$getPrototypeO;
+
+    _classCallCheck(this, MyArray);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(MyArray)).call.apply(_Object$getPrototypeO, [this].concat(args)));
+  }
+
+  return MyArray;
+}(Array);
+
+// Two-phase 'new':
+// 1) Call @@create to allocate object
+// 2) Invoke constructor on new instance
+
+var arr = new MyArray();
+arr[1] = 12;
+arr.length == 2;
+
+// ============================== Math + Number + String + Array + Object APIs =========================== \\
+Number.EPSILON;
+Number.isInteger(Infinity); // false
+Number.isNaN("NaN"); // false
+
+Math.acosh(3); // 1.762747174039086
+Math.hypot(3, 4); // 5
+Math.imul(Math.pow(2, 32) - 1, Math.pow(2, 32) - 2); // 2
+
+"abcde".includes("cd"); // true
+"abc".repeat(3); // "abcabcabc"
+
+Array.from(document.querySelectorAll('*')); // Returns a real Array
+Array.of(1, 2, 3) // Similar to new Array(...), but without special one-arg behavior
+[(0, 0, 0)].fill(7, 1) // [0,7,7]
+[(1, 2, 3)].find(function (x) {
+  return x == 3;
+}) // 3
+[(1, 2, 3)].findIndex(function (x) {
+  return x == 2;
+}) // 1
+[(1, 2, 3, 4, 5)].copyWithin(3, 0) // [1, 2, 3, 1, 2]
+[("a", "b", "c")].entries() // iterator [0, "a"], [1,"b"], [2,"c"]
+[("a", "b", "c")].keys() // iterator 0, 1, 2
+[("a", "b", "c")].values(); // iterator "a", "b", "c"
+
+Object.assign(Point, { origin: new Point(0, 0) });
+
+// ============================== Binary and Octal Literals =========================== \\
+503 === 503; // true
+503 === 503; // true
+// ============================== Promises =========================== \\
+function timeout() {
+  var duration = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, duration);
+  });
+}
+
+var p = timeout(1000).then(function () {
+  return timeout(2000);
+}).then(function () {
+  throw new Error("hmm");
+}).catch(function (err) {
+  return Promise.all([timeout(100), timeout(200)]);
+});
+
+// ============================== Reflect API =========================== \\
+// No sample yet
+
+// ============================== Tail Calls =========================== \\
+function factorial(n) {
+  'use strict';
+
+  var acc = arguments.length <= 1 || arguments[1] === undefined ? 1 : arguments[1];
+  if (n <= 1) return acc;
+  return factorial(n - 1, n * acc);
+}
+
+// Stack overflow in most implementations today,
+// but safe on arbitrary inputs in ES6
+factorial(100000);
 //# sourceMappingURL=app.js.map
--- src/app.js
+++ src/app.js
@@ -1,2 +1,337 @@
-const p = 3.14;
+// ============================== Arrows =========================== \\
+// Expression bodies
+var odds = evens.map(v => v + 1);
+var nums = evens.map((v, i) => v + i);
+var pairs = evens.map(v => ({even: v, odd: v + 1}));
 
+// Statement bodies
+nums.forEach(v => {
+  if (v % 5 === 0)
+    fives.push(v);
+});
+
+// Lexical this
+var bob = {
+  _name: "Bob",
+  _friends: [],
+  printFriends() {
+    this._friends.forEach(f =>
+      console.log(this._name + " knows " + f));
+  }
+}
+// ============================== Classes =========================== \\
+class SkinnedMesh extends THREE.Mesh {
+  constructor(geometry, materials) {
+    super(geometry, materials);
+
+    this.idMatrix = SkinnedMesh.defaultMatrix();
+    this.bones = [];
+    this.boneMatrices = [];
+    //...
+  }
+  update(camera) {
+    //...
+    super.update();
+  }
+  get boneCount() {
+    return this.bones.length;
+  }
+  set matrixType(matrixType) {
+    this.idMatrix = SkinnedMesh[matrixType]();
+  }
+  static defaultMatrix() {
+    return new THREE.Matrix4();
+  }
+}
+
+// ============================== Enhanced Object Literals =========================== \\
+var obj = {
+    // __proto__
+    __proto__: theProtoObj,
+    // Shorthand for ‘handler: handler’
+    handler,
+    // Methods
+    toString() {
+     // Super calls
+     return "d " + super.toString();
+    },
+    // Computed (dynamic) property names
+    [ 'prop_' + (() => 42)() ]: 42
+};
+
+// ============================== Template Strings =========================== \\
+// Basic literal string creation
+`In JavaScript '\n' is a line-feed.`
+
+// Multiline strings
+`In JavaScript this is
+ not legal.`
+
+// String interpolation
+var name = "Bob", time = "today";
+`Hello ${name}, how are you ${time}?`
+
+// Construct an HTTP request prefix is used to interpret the replacements and construction
+POST`http://foo.org/bar?a=${a}&b=${b}
+     Content-Type: application/json
+     X-Credentials: ${credentials}
+     { "foo": ${foo},
+       "bar": ${bar}}`(myOnReadyStateChangeHandler);
+
+// ============================== Destructuring =========================== \\
+// list matching
+var [a, , b] = [1,2,3];
+
+// object matching
+var { op: a, lhs: { op: b }, rhs: c }
+       = getASTNode()
+
+// object matching shorthand
+// binds `op`, `lhs` and `rhs` in scope
+var {op, lhs, rhs} = getASTNode()
+
+// Can be used in parameter position
+function g({name: x}) {
+  console.log(x);
+}
+g({name: 5})
+
+// Fail-soft destructuring
+var [a] = [];
+a === undefined;
+
+// Fail-soft destructuring with defaults
+var [a = 1] = [];
+a === 1;
+
+// ============================== Default + Rest + Spread =========================== \\
+function f(x, y=12) {
+  // y is 12 if not passed (or passed as undefined)
+  return x + y;
+}
+f(3) == 15
+function f(x, ...y) {
+  // y is an Array
+  return x * y.length;
+}
+f(3, "hello", true) == 6
+function f(x, y, z) {
+  return x + y + z;
+}
+// Pass each elem of array as argument
+f(...[1,2,3]) == 6
+
+// ============================== Let + Const =========================== \\
+
+
+// ============================== Iterators + For..Of =========================== \\
+let fibonacci = {
+  [Symbol.iterator]() {
+    let pre = 0, cur = 1;
+    return {
+      next() {
+        [pre, cur] = [cur, pre + cur];
+        return { done: false, value: cur }
+      }
+    }
+  }
+}
+
+for (var n of fibonacci) {
+  // truncate the sequence at 1000
+  if (n > 1000)
+    break;
+  console.log(n);
+}
+
+// ============================== Generators =========================== \\
+
+var fibonacciX = {
+  [Symbol.iterator]: function*() {
+    var pre = 0, cur = 1;
+    for (;;) {
+      var temp = pre;
+      pre = cur;
+      cur += temp;
+      yield cur;
+    }
+  }
+}
+
+for (var n of fibonacciX) {
+  // truncate the sequence at 1000
+  if (n > 1000)
+    break;
+  console.log(n);
+}
+// ============================== Unicode =========================== \\
+// same as ES5.1
+"𠮷".length == 2
+
+// new RegExp behaviour, opt-in ‘u’
+"𠮷".match(/./u)[0].length == 2
+
+// new form
+"\u{20BB7}"=="𠮷"=="\uD842\uDFB7"
+
+// new String ops
+"𠮷".codePointAt(0) == 0x20BB7
+
+// for-of iterates code points
+for(var c of "𠮷") {
+  console.log(c);
+}
+
+
+// ============================== Module Loaders =========================== \\
+
+
+// ============================== Map + Set + WeakMap + WeakSet =========================== \\
+// Sets
+var s = new Set();
+s.add("hello").add("goodbye").add("hello");
+s.size === 2;
+s.has("hello") === true;
+
+// Maps
+var m = new Map();
+m.set("hello", 42);
+m.set(s, 34);
+m.get(s) == 34;
+
+// Weak Maps
+var wm = new WeakMap();
+wm.set(s, { extra: 42 });
+wm.size === undefined
+
+// Weak Sets
+var ws = new WeakSet();
+ws.add({ data: 42 });
+// Because the added object has no other references, it will not be held in the set
+
+
+// ============================== Proxies =========================== \\
+// Proxying a normal object
+var target = {};
+var handler = {
+  get: function (receiver, name) {
+    return `Hello, ${name}!`;
+  }
+};
+
+var p = new Proxy(target, handler);
+p.world === 'Hello, world!';
+
+
+// Proxying a function object
+var target = function () { return 'I am the target'; };
+var handler = {
+  apply: function (receiver, ...args) {
+    return 'I am the proxy';
+  }
+};
+
+var p = new Proxy(target, handler);
+p() === 'I am the proxy';
+
+
+// ============================== Symbols =========================== \\
+var MyClass = (function() {
+
+  // module scoped symbol
+  var key = Symbol("key");
+
+  function MyClass(privateData) {
+    this[key] = privateData;
+  }
+
+  MyClass.prototype = {
+    doStuff: function() {
+      this[key]
+    }
+  };
+
+  return MyClass;
+})();
+
+var c = new MyClass("hello")
+c["key"] === undefined
+
+// ============================== Subclassable Built-ins =========================== \\
+// Pseudo-code of Array
+class Array {
+    constructor(...args) { /* ... */ }
+    static [Symbol.create]() {
+        // Install special [[DefineOwnProperty]]
+        // to magically update 'length'
+    }
+}
+
+// User code of Array subclass
+class MyArray extends Array {
+    constructor(...args) { super(...args); }
+}
+
+// Two-phase 'new':
+// 1) Call @@create to allocate object
+// 2) Invoke constructor on new instance
+var arr = new MyArray();
+arr[1] = 12;
+arr.length == 2
+
+// ============================== Math + Number + String + Array + Object APIs =========================== \\
+Number.EPSILON
+Number.isInteger(Infinity) // false
+Number.isNaN("NaN") // false
+
+Math.acosh(3) // 1.762747174039086
+Math.hypot(3, 4) // 5
+Math.imul(Math.pow(2, 32) - 1, Math.pow(2, 32) - 2) // 2
+
+"abcde".includes("cd") // true
+"abc".repeat(3) // "abcabcabc"
+
+Array.from(document.querySelectorAll('*')) // Returns a real Array
+Array.of(1, 2, 3) // Similar to new Array(...), but without special one-arg behavior
+[0, 0, 0].fill(7, 1) // [0,7,7]
+[1, 2, 3].find(x => x == 3) // 3
+[1, 2, 3].findIndex(x => x == 2) // 1
+[1, 2, 3, 4, 5].copyWithin(3, 0) // [1, 2, 3, 1, 2]
+["a", "b", "c"].entries() // iterator [0, "a"], [1,"b"], [2,"c"]
+["a", "b", "c"].keys() // iterator 0, 1, 2
+["a", "b", "c"].values() // iterator "a", "b", "c"
+
+Object.assign(Point, { origin: new Point(0,0) })
+
+// ============================== Binary and Octal Literals =========================== \\
+0b111110111 === 503 // true
+0o767 === 503 // true
+// ============================== Promises =========================== \\
+function timeout(duration = 0) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, duration);
+    })
+}
+
+var p = timeout(1000).then(() => {
+    return timeout(2000);
+}).then(() => {
+    throw new Error("hmm");
+}).catch(err => {
+    return Promise.all([timeout(100), timeout(200)]);
+})
+
+// ============================== Reflect API =========================== \\
+// No sample yet
+
+
+// ============================== Tail Calls =========================== \\
+function factorial(n, acc = 1) {
+    'use strict';
+    if (n <= 1) return acc;
+    return factorial(n - 1, n * acc);
+}
+
+// Stack overflow in most implementations today,
+// but safe on arbitrary inputs in ES6
+factorial(100000)
